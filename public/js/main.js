e99b2c1aeea95e42e0ee87fe57e7d8cda49ebf73
satul23 <satul3868@gmail.com>
2023-06-19 16:30:05 +0530

    weatherproject

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,7 @@ const city_name = document.getElementById('city_name');
 const temp_status = document.getElementById('temp_status');
 const temp_real_val = document.getElementById('temp_real_val');
 
+const datahide = document.querySelector(".middle_layer")
 
 const getinfo = async(event) =>{
     event.preventDefault();
@@ -13,6 +14,7 @@ const getinfo = async(event) =>{
     
    if(cityval === ""){
       city_name.innerText = `please write the name before search`;
+      datahide.classList.add('data_hide');
    }else{
     try{
     let url =  `https://api.openweathermap.org/data/2.5/weather?q=${cityval}&units=metric&appid=fb639b8ca84772497886688ea5a7b755`
@@ -24,8 +26,10 @@ const getinfo = async(event) =>{
     city_name.innerText = `${arrData[0].name} , ${arrData[0].sys.country}`;
     temp_real_val.innerText = arrData[0].main.temp;
     // temp_status.innerText = arrData[0].weather[0].main;
+    datahide.classList.remove('data_hide');
    }catch{
       city_name.innerText = `please enter the city name properly`
+      datahide.classList.add('data_hide');
    }
 }
 }  
